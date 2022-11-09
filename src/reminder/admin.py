f177f63724ce1854d6788483ef31ea1caf4694ce
pemidi <rashidi.peyman@gmail.com>
2022-11-09 23:28:36 +0330

    feat: add Reminder App Models to Admin

--- src/reminder/admin.py
+++ src/reminder/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from reminder.models import Alert, Category, Reminder, ReminderCategory
+
+
+class AlertInline(admin.TabularInline):
+    model = Alert
+
+
+class ReminderCategoryInline(admin.TabularInline):
+    model = ReminderCategory
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title']
+
+    inlines = [ReminderCategoryInline]
+
+
+@admin.register(Reminder)
+class ReminderAdmin(admin.ModelAdmin):
+    list_display = ['title', 'scheduled_at']
+    search_fields = ['title', 'message']
+
+    inlines = [AlertInline]
+
+
+
